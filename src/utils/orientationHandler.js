009e0a7f38c7d55757d614a31f0d7a6b428eea96
KooMar <djhracak@gmail.com>
2024-11-29 15:44:19 +0100

    commit - attempt to lock screen orientation change on mobile devices.

--- src/utils/orientationHandler.js
+++ src/utils/orientationHandler.js
@@ -3,20 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
         if (screen.orientation && screen.orientation.lock) {
             try {
                 await screen.orientation.lock("portrait");
-                console.log("Orientation locked to portrait");
-            } catch (err) {
-                console.warn("Failed to lock orientation:", err);
+                console.log("Orientation locked to portrait.");
+            } catch (error) {
+                console.warn("Could not lock the orientation:", error);
             }
         } else {
             console.warn("Screen Orientation API is not supported on this browser.");
         }
     };
-
-    const isMobile = () => /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-
-    if (isMobile()) {
-        document.addEventListener("click", () => {
-            lockOrientation();
-        });
-    }
+    document.addEventListener("click", () => {
+        lockOrientation();
+    });
 });
