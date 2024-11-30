eb4109a603c236ad98f6d6fef07036fb7698e7f4
KooMar <djhracak@gmail.com>
2024-11-30 09:35:06 +0100

    commit - removed scrolling to selected song for now, since it fucks up centering

--- src/components/SongsList.jsx
+++ src/components/SongsList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState} from "react";
 import "../songslist.css";
 import timer from "../utils/timer";
 import { useGenres } from "../services/useGenres";
@@ -13,9 +13,6 @@ const SongsList = ({ open, musicNumber, setMusicNumber, setSongs }) => {
     // Fetch genres and songs
     const { data: genres, isLoading: loadingGenres } = useGenres();
     const { data: songs, isLoading: loadingSongs, error: songsError } = useSongs(selectedGenre);
-
-    // Create a ref for each song
-    const songRefs = useRef([]);
 
     useEffect(() => {
         if (songs) {
@@ -38,16 +35,6 @@ const SongsList = ({ open, musicNumber, setMusicNumber, setSongs }) => {
             loadMetadata();
         }
     }, [musicNumber, loadedSongs]);
-
-    // Scroll to current song
-    useEffect(() => {
-        if (open && musicNumber !== null && songRefs.current[musicNumber]) {
-            songRefs.current[musicNumber].scrollIntoView({
-                behavior: "smooth",
-                block: "nearest",
-            });
-        }
-    }, [open, musicNumber]);
 
     // Pause playback and reset the song number to null when the genre changes
     const handleGenreChange = (e) => {
@@ -124,7 +111,6 @@ const SongsList = ({ open, musicNumber, setMusicNumber, setSongs }) => {
                 {loadedSongs && loadedSongs.length > 0 && loadedSongs.map((song, index) => (
                     <li
                         key={song.id}
-                        ref={(element) => (songRefs.current[index] = element)}
                         onClick={() => handleSongSelect(index)}
                         className={`${musicNumber === index ? "playing" : ""}`}
                     >
